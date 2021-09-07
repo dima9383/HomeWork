ae942ba994075b9276dea9cb21a4f5b4ecc0310d
555 <dima9383@gmail.com>
2021-09-07 17:48:51 +0300

    5

--- HomeWork_1/src/com/company/Task_5.java
+++ HomeWork_1/src/com/company/Task_5.java
@@ -3,13 +3,13 @@ package com.company;
 public class Task_5 {
     public static void main(String[] args) {
 
-        int x = 5;
-        int y = 5;
+        int x = 7;
+        int y = 7;
 
         int array[][] = new int[x][y];
 
         boolean needLowTriangle;
-        boolean needBrake;
+        boolean needBrake = false;
 
         needLowTriangle = true;
 
@@ -20,6 +20,9 @@ public class Task_5 {
                 if (needLowTriangle && i < j) {
                     array[i][j] = 0;
                 }
+                if (i == 2 && j == 5) {
+                    array[i][j] = 77;
+                }
                 System.out.print(array[i][j] + " " + (array[i][j] < 10 ? " " : ""));
             }
             System.out.println();
@@ -28,21 +31,24 @@ public class Task_5 {
 
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
-                if (i < j && array[i][j] == 0) {
+                if (i < j && array[i][j] != 0) {
                     needBrake = true;
+                    System.out.println("i=" + i  +" j=" + j + ": " + array[i][j]);
+                    System.out.print("не ");
                     break;
                 }
-                if (needBrake){
-                    break;
-                }
-                if (i > j){
+
+                if (i < j) {
                     continue;
                 }
+            }
+            if (needBrake) {
+                break;
             }
         }
 
 
-        System.out.println();
+        System.out.println("является нижней треугольной матрицей");
 
     }
 }
